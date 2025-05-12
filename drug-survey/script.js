146962d1c90936bf7e19150e799e9403d92efd74
yeshixin <yeshixin1207@gmail.com>
2025-05-12 13:51:45 +0800

    Add files via upload

---
+++ drug-survey/script.js
@@ -0,0 +1,5 @@
+const colorPairs = [
+    { left: { text: 'WHITE', bg: 'red', color: 'white' }, right: { text: 'WHITE', bg: 'blue', color: 'white' } },
+    { left: { text: 'BLACK', bg: 'yellow', color: 'black' }, right: { text: 'BLACK', bg: 'green', color: 'black' } }
+  ];
+  
